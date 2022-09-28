782a8763596d0e77cf68d91f54b5e5933f8c0a94
Kilisi <kilisivazquez@yahoo.com>
2022-09-27 21:23:07 -0700

    fixed routes

--- Main/routes/api/index.js
+++ Main/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const courseRoutes = require('./thoughtRoutes');
 const studentRoutes = require('./userRoutes');
 
-router.use('/thoughts', courseRoutes);
-router.use('/users', studentRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
